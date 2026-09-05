c024d13ddf609e2f7fdc8f3dae12e8023de08361
YunosukeY <yamada.yunosuke.1123@gmail.com>
2026-09-05 20:47:34 +0900

    ボタン確認

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,23 @@ void setup() {
   auto cfg = M5.config();
   M5.begin(cfg);
 
-  M5.Display.setTextSize(2);
-  M5.Display.println("Hello World");
+  Serial.println("Press BtnA/B");
 }
 
 void loop() {
   M5.update();
+
+  // wasClicked() は長押し閾値(既定 500ms)未満で離したとき、
+  // wasHold() は押下が閾値を超えた時点で 1 度だけ true になる。
+  if (M5.BtnA.wasClicked()) {
+    Serial.println("BtnA short");
+  } else if (M5.BtnA.wasHold()) {
+    Serial.println("BtnA long");
+  } else if (M5.BtnB.wasClicked()) {
+    Serial.println("BtnB short");
+  } else if (M5.BtnB.wasHold()) {
+    Serial.println("BtnB long");
+  }
+
   delay(10);
 }
